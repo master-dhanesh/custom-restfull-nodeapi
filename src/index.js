9909111e670b23f9b43cc07d719c0e2b4fb0766f
master-dhanesh <dhanesh1296@gmail.com>
2024-09-21 15:59:16 +0530

    routers db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const dotenv = require("dotenv");
+const path = require("path");
+const app = require("./app");
+const { connectDB } = require("./db");
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
