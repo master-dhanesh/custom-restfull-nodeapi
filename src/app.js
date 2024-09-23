bb4288029cd7b1b589ce2dbde9e1e20bbe203bdd
master-dhanesh <dhanesh1296@gmail.com>
2024-09-23 15:42:58 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/index.routes");
+
+app.use(cors({ origin: true, credentials: true }));
 
 app.use("/api/v1", indexRouter);
 
